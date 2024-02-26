226ddd627bbb30406983c182219cb30c4982ec29
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2024-02-26 17:03:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function validEmail(str) {
   //your JS code here.
+	const size = str.length;
+	if(size < 5 || str[0] == '@' || str[size-1]=='.') return false;
+	let at = str.indexOf('@');
+
+	for(let index=size-1; index>=at; index--){
+		if(str[index]=='.') return true;
+	}
+
+	return false;
 }
 
 // Do not change the code below.
